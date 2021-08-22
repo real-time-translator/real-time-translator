137e1c1518fdda2e97f02551dd0ed87e72fd444d
Anas AbuGhalieh <civil.Anas1000@gmail.com>
2021-08-22 07:23:49 -0700

    feature:adding transcript from record

--- real_time_translator/voice.py
+++ real_time_translator/voice.py
@@ -1,27 +1,44 @@
+from typing import Text
 import speech_recognition as sr
-from translate import Translator
 import audiomath as am
+import ffmpeg
 r = sr.Recognizer()
 
 
-def translate_from_file(file:str,to_lang="en")->str:
+def transcript_from_file(file:str)->str:
     # select the language you want to translate to 
-    translator= Translator(to_lang=to_lang)
     with sr.AudioFile(file) as source:
       # listen for the data (load audio to memory)
       audio_data = r.record(source)
       # recognize (convert from speech to text)
       text = r.recognize_google(audio_data)
-      # translation = translator.translate("hallo meine Freunde")
-      print(text)
+      
+    return(text)
 
 
 
-def translate_from_record(to_lang="en"):
+def transcript_from_record():
   print("start talking ")
-  s = am.Record() 
+
+  s = am.Record(10) 
+  s.Write('source/file.wav')
+  file='source/file.wav'
+  text=transcript_from_file(file)
+  print(text)
+  
+  # translate_from_file(file,'ar')
+
+
+
+  
+
 
 if __name__ == '__main__':
   file='source/audio_files_harvard.wav'
-  # translate_from_file(file,'ar')
-  translate_from_record()
+  transcript_from_record()
+
+
+
+
+
+
