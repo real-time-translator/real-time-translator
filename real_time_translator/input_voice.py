7c09dfb26d537ade9394d7c38d3b44c02dc298a8
Anas AbuGhalieh <civil.Anas1000@gmail.com>
2021-08-23 10:40:22 -0700

    fix bug with handle mp3 file

--- real_time_translator/input_voice.py
+++ real_time_translator/input_voice.py
@@ -11,7 +11,10 @@ def transcript_from_file(file:str)->str:
 
     # convert the mp3 file to wav 
     if file.endswith('.mp3'):
-      os.remove('assets/voices/file_converted.wav')
+      try:
+       os.remove('assets/voices/file_converted.wav')
+      except:
+        pass
       subprocess.call(['ffmpeg', '-i', file,
                  'assets/voices/file_converted.wav'])
       #save Audio  in file 
@@ -37,11 +40,11 @@ def transcript_from_record():
   print(text)
   return(text)
   
-  
-# if __name__ == '__main__':
-#   file='source/audio_files_harvard.wav'
-#   print(transcript_from_file('assets/voices/How to Give Welcome Remarks.mp3'))
-#   # print(transcript_from_record())
+
+if __name__ == '__main__':
+  file='source/audio_files_harvard.wav'
+  print(transcript_from_file('assets/voices/How to Give Welcome Remarks.mp3'))
+  # print(transcript_from_record())
 
 
 
