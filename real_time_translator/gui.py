a0ca3326437ff45d1dab8680dbcb979ffdd85c62
nuratabanjeh <nuratabanjeh96@gmail.com>
2021-08-23 22:33:34 +0300

    main page classes

--- real_time_translator/gui.py
+++ real_time_translator/gui.py
@@ -1,30 +1,67 @@
+from input_text import input_text_manually, input_text_file
+from input_image import imagetotext
+from input_voice import transcript_from_file, transcript_from_record
+from edit_text import edit_text
 import tkinter as tk
-def page1(root):
-    # root.title("Real Time Translator")
-    page = tk.Frame(root)
-    page.grid()
-    tk.Label(page, text = 'This is page 1').grid(row = 0)
-    tk.Button(page, text = 'To page 2', command = changepage).grid(row = 1)
-    
-def page2(root):
-    page = tk.Frame(root)
-    page.grid()
-    tk.Label(page, text = 'This is page 2').grid(row = 0)
-    tk.Button(page, text = 'To page 1', command = changepage).grid(row = 1)
+class Page(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+    def show(self):
+        self.lift()
 
-def changepage():
-    global pagenum, root
-    for widget in root.winfo_children():
-        widget.destroy()
-    if pagenum == 1:
-        page2(root)
-        pagenum = 2
-    else:
-        page1(root)
-        pagenum = 1
+class Page1(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 1")
+       label.pack(side="top", fill="both", expand=True)
+       button_record=tk.Button(self,text="Start Recording",command=transcript_from_record)
+       button_record.grid(row=1,column=1)
 
-pagenum = 1
-root = tk.Tk()
-root.title("Real Time Translator")
-page1(root)
-root.mainloop()
+
+class Page2(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 2")
+       label.pack(side="top", fill="both", expand=True)
+
+
+class Page3(Page):
+   def __init__(self, *args, **kwargs):
+       Page.__init__(self, *args, **kwargs)
+       label = tk.Label(self, text="This is page 3")
+       label.pack(side="top", fill="both", expand=True)
+
+class MainView(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+        p1 = Page1(self)
+        p2 = Page2(self)
+        p3 = Page3(self)
+
+        buttonframe = tk.Frame(self)
+        container = tk.Frame(self)
+        buttonframe.pack(side="top", fill="x", expand=False)
+        container.pack(side="top", fill="both", expand=True)
+
+        p1.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p2.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+        p3.place(in_=container, x=0, y=0, relwidth=1, relheight=1)
+
+        b1 = tk.Button(buttonframe, text="Page 1", command=p1.show)
+        b2 = tk.Button(buttonframe, text="Page 2", command=p2.show)
+        b3 = tk.Button(buttonframe, text="Page 3", command=p3.show)
+
+        b1.pack(side="left")
+        b2.pack(side="left")
+        b3.pack(side="left")
+
+        p1.show()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Real Time Translator")
+    main = MainView(root)
+    main.pack(side="top", fill="both", expand=True)
+    root.wm_geometry("400x400")
+    root.mainloop()
