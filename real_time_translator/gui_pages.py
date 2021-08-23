77ef531f1e1d3c8d8de4931f764c57613f1569dc
Anas AbuGhalieh <civil.Anas1000@gmail.com>
2021-08-23 14:21:28 -0700

    voice recognation Page almost done

--- real_time_translator/gui_pages.py
+++ real_time_translator/gui_pages.py
@@ -1,3 +1,5 @@
+from tkinter.constants import END, S
+from typing import Text
 from input_text import input_text_manually, input_text_file
 from input_image import imagetotext
 from input_voice import transcript_from_file, transcript_from_record
@@ -16,20 +18,94 @@ class Page(tk.Frame):
 
 class Page1(Page):
     def __init__(self, *args, **kwargs):
-       extracted_text=''
        Page.__init__(self, *args, **kwargs)
+      
+       self.extracted_text=None
+       self.edit_text=None
+
        label = tk.Label(self, text="This is page 1")
        label.grid(row=12,column=12)
-       button_record=tk.Button(self,text="Start Recording",command=self.handle_call)
-       button_record.grid(row=1,column=1)
-       label = tk.Label(self, text=extracted_text)
-       label.grid(row=9,column=9)
+
+       self.button_record=tk.Button(self,text="Start Recording",command=self.handle_record)
+       self.button_record.place(x=130,y=30)
+
+       
+
+       
+       
+      
+       
      
    
-    def handle_call(self):
-       self.extracted_text=transcript_from_record()
-       self.show()
-      
+    def handle_record(self):
+
+        try:
+            self.box.destroy()
+        except:
+                
+            if not self.extracted_text: 
+
+                self.extracted_text=transcript_from_record()
+                self.label = tk.Label(self, text=self.extracted_text)
+                self.label .place(x=130,y=120)
+            
+
+            else :
+                self.label .destroy()
+                self.extracted_text=transcript_from_record()
+                self.label  = tk.Label(self, text=self.extracted_text)
+                self.label .place(x=130,y=120)
+                
+            edit_button=tk.Button(self,text="Edit Text",command=self.handle_editing)
+            edit_button.place(x=20,y=120)
+            translate_button=tk.Button(self,text="Translate",command=self.handle_translation)
+            translate_button.place(x=20,y=170)
+
+
+    def handle_editing(self):
+
+            self.user_input = tk.StringVar(self)
+            self.box=tk.Entry(self,textvariable=self.user_input)
+
+            self.box.insert(END,self.extracted_text)
+            self.box.place(x=130,y=150)
+
+            self.submit_button=tk.Button(self,text=" Submit ",command=self.handle_submiting)
+            self.submit_button.place(x=20,y=150)
+
+            self.button_record=tk.Button(self,text="Start Recording",command=self.handle_record)
+            self.button_record.place(x=130,y=30)
+
+            
+    
+        
+     
+
+
+    def handle_submiting(self):
+        self.extracted_text=self.user_input.get()
+        print(self.extracted_text)
+        self.user_input.set("")
+        self.label .destroy()
+        self.label  = tk.Label(self, text=self.extracted_text)
+        self.label .place(x=130,y=120)
+        self.submit_button.destroy()
+        self.box.destroy()
+
+
+    def handle_translation(self):
+
+        try:
+            self.label_translation.destroy()
+            self.label_translation  = tk.Label(self, text=self.extracted_text)
+            self.label_translation .place(x=130,y=200)
+           
+        except:
+
+            self.label_translation  = tk.Label(self, text=self.extracted_text)
+            self.label_translation .place(x=130,y=200)
+            print('b')
+
   
 
 class Page2(Page):
