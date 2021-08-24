3b4dde1c3921efda467e3ee34a3a2ad1713b0f87
mahmoud-salameh <mahmoud4561@hotmail.com>
2021-08-24 18:52:58 +0300

    page-3 functionality almost done need some work on translator button

--- real_time_translator/gui_files_page.py
+++ real_time_translator/gui_files_page.py
@@ -2,24 +2,19 @@ import os
 from input_text import input_text_file
 from input_image import imagetotext
 from input_voice import transcript_from_file
-# from edit_text import edit_text
 from tkinter import *
 from tkinter.ttk import *
 from tkinter.filedialog import askopenfile ,askopenfilename
 from tkinter import filedialog
 from translator import translat_str
-# from googletrans import Translator
 
 root = Tk()
 root.geometry('1050x400')
 root.resizable(0,0)
 root.config(bg = 'ghost white')
 root.title('Real-Time-Translator')
-
 extracted_text=None
 user_input = StringVar()
-
-
 
 def ask_for_image():
     global extracted_text
@@ -39,7 +34,6 @@ def ask_for_text():
                ('text files', '.txt'),
               ])
     extracted_text = input_text_file(file_path.name)
-    # print (extracted_text)
     show_answer(extracted_text)
 
 def ask_for_audio():
@@ -51,7 +45,6 @@ def ask_for_audio():
            ])
     extracted_text = transcript_from_file(file_path.name)
     show_answer(extracted_text)
-
 
 def show_answer(extracted_text):
     label = Label(text=extracted_text)
@@ -69,35 +62,19 @@ def ask_for_edit():
         extracted_text=user_input.get()
         user_input.set("")
         print(extracted_text)
-
         label  = Label( text=extracted_text)
         label.place(x=300,y = 160 ,height = 75,width = 450)
         submit_button.destroy()
-        # box.destroy()
-        # show_answer(extracted_text)
-
     submit_button=Button(root,text="Submit ",command=ask_for_submit)
     submit_button.place(x = 800, y= 190 )
 
 def translate():
     global extracted_text
     translated_text = translat_str(extracted_text)
-    # Input_text2 = Text(root, height = 5, wrap = WORD, padx=5, pady=5, width = 60)
-    label = Label(text=translated_text)
-    label.place(x=300,y = 300)
+    print(translated_text)
+    label_translated = Label(root,text=translated_text)
+    label_translated.place(x=300,y = 300, height = 75,width = 450)
 
-
-#     # Initial
-#     translator = googletrans.Translator()
-#     # Basic Translate
-#     translation = translator.translate("sara", dest="ar")
-#     translated = f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})"
-#     print(translated)
-#
-#     # with open('abc.txt',mode ='w') as file:
-#     #         file.write(translated)
-#
-#     return translated
 
 adharbtn = Button(root,text ='Choose a picture',command = lambda:ask_for_image()) 
 adharbtn.place(x=250,y = 120)
@@ -111,24 +88,8 @@ msbtn.place(x=650,y = 120)
 trans_btn = Button(root, text = 'Edit',command = ask_for_edit)
 trans_btn.place(x = 800, y= 190 )
 
-trans_btn = Button(root, text = 'Translate',command = translate())
+trans_btn = Button(root, text = 'Translate',command = translate)
 trans_btn.place(x = 450, y= 250 )
 
 
-
 root.mainloop()
-
-
-# trans_btn = tk.Button( text = 'Edit Text',font = 'arial 12 bold',pady = 5,command = input , bg = 'blue', activebackground = 'sky blue')
-# trans_btn.place(x = 800, y= 190 )
-# Input_text = Text(font = 'arial 10', height = 5, wrap = WORD, padx=5, pady=5, width = 60)
-# Input_text.place(x=300,y = 160)
-# # #Import the image using PhotoImage function
-# # click_btn= imagetotext(file='clickme.png')
-# # #Let us create a label for tk.button event
-# # img_label= Label(image=click_btn)
-# # #Let us create a dummy button and pass the image
-# # button= Button(win, image=click_btn,command= my_command,
-# # borderwidth=0)
-# # button.pack(pady=30)
-
--- real_time_translator/translator.py
+++ real_time_translator/translator.py
@@ -1,15 +1,20 @@
 import googletrans
 
 def translat_str(snetence,to_language='ar'):
-
-    # Initial
     translator = googletrans.Translator()
-    # Basic Translate
     translation = translator.translate(snetence, dest=to_language)
     translated = f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})"
+    
+    with open('real_time_translator/abc.txt',mode ='w') as file:     
+        file.write(translated)
+    # with open('real_time_translator/abc.txt',mode='r') as file: 
+    #     translated = file.readlines()
+    
+    file = open('real_time_translator/abc.txt','r')
+    translated = file.read()
     print(translated)
     return translated
 
 if __name__ == "__main__":
-    print(translat_str("تفاحة",'en').lower())
-    # pass
+    print(translat_str("تفاحة",'en'))
+    pass
