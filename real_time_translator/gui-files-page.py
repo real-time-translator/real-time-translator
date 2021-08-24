fc425a77aa67ea352c1569f8a14d97345bd185a5
Yahia Qous <yahiaqous@gmail.com>
2021-08-24 18:01:51 +0300

    Edit and Submit functions are done for GUI

--- real_time_translator/gui-files-page.py
+++ real_time_translator/gui-files-page.py
@@ -7,8 +7,8 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter.filedialog import askopenfile ,askopenfilename
 from tkinter import filedialog
-from translator import *
-from googletrans import Translator
+from translator import translat_str
+# from googletrans import Translator
 
 root = Tk()
 root.geometry('1050x400')
@@ -17,23 +17,12 @@ root.config(bg = 'ghost white')
 root.title('Real-Time-Translator')
 
 extracted_text=None
-edit_text=None
+user_input = StringVar()
 
-# def translate():
-#     # Initial
-#     translator = googletrans.Translator()
-#     # Basic Translate
-#     translation = translator.translate("sara", dest="ar")
-#     translated = f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})"
-#     print(translated)
-#
-#     # with open('abc.txt',mode ='w') as file:
-#     #         file.write(translated)
-#
-#     return translated
 
 
 def ask_for_image():
+    global extracted_text
     file_path = filedialog.askopenfile(   title="Choose a file",
     filetypes=[
                ('image files', '.png'),
@@ -54,6 +43,7 @@ def ask_for_text():
     show_answer(extracted_text)
 
 def ask_for_audio():
+    global extracted_text
     file_path = filedialog.askopenfile(   title="Choose a file",
     filetypes=[
                ('audio', '.wav'),
@@ -64,59 +54,50 @@ def ask_for_audio():
 
 
 def show_answer(extracted_text):
-    # Input_text = Entry(root)
-    # text=Entry(root,  text = "%s" %(Input_text) )
-    # text  = Label(root, text=extracted_text)
     label = Label(text=extracted_text)
     label .place(x=300,y = 160 ,height = 75,width = 450)
-
-    # text.place(x=300,y = 160 ,height = 75,width = 450)
-    # text.insert(END,extracted_text)
     print(extracted_text)
 
 def ask_for_edit():
-    # extracted= Entry(root)
-    # box=Entry(root,text=extracted_text)
-    # box.insert(END,str(extracted_text))
-    # box.place(x=300,y = 160 ,height = 75,width = 450)
-    # user_input = StringVar()
-    global extracted_text
-    box=Entry(textvariable=extracted_text)
+    global extracted_text, user_input
+    box=Entry(root,textvariable=user_input)
     box.insert(END,extracted_text)
     box.place(x=300,y = 160 ,height = 75,width = 450)
+
+    def ask_for_submit():
+        global extracted_text, user_input
+        extracted_text=user_input.get()
+        user_input.set("")
+        print(extracted_text)
+
+        label  = Label( text=extracted_text)
+        label.place(x=300,y = 160 ,height = 75,width = 450)
+        submit_button.destroy()
+        # box.destroy()
+        # show_answer(extracted_text)
+
     submit_button=Button(root,text="Submit ",command=ask_for_submit)
     submit_button.place(x = 800, y= 190 )
-    # submit_button=Button(text=" Submit ",command=handle_submiting)
-    # submit_button.place(x=20,y=150)
 
-def ask_for_submit():
-    # pass
+def translate():
     global extracted_text
-    
-    print(extracted_text)
-    
-    label  = Label( text=extracted_text)
-    label.place(x=300,y = 160 ,height = 75,width = 450)
-    label .destroy()
-    submit_button.destroy()
-    box.destroy()
+    translated_text = translat_str(extracted_text)
+    # Input_text2 = Text(root, height = 5, wrap = WORD, padx=5, pady=5, width = 60)
+    label = Label(text=translated_text)
+    label.place(x=300,y = 300)
 
 
-#         Input_text = Entry(root)
+#     # Initial
+#     translator = googletrans.Translator()
+#     # Basic Translate
+#     translation = translator.translate("sara", dest="ar")
+#     translated = f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})"
+#     print(translated)
 #
-#         text=Entry(root,  text = "%s" %(Input_text) )
+#     # with open('abc.txt',mode ='w') as file:
+#     #         file.write(translated)
 #
-#         text.insert(END,extracted_text)
-#         text.place(x=130,y=150)
-#
-#         submit_button=root.Button(text=" Submit ",command=handle_submiting)
-#         submit_button.place(x=20,y=150)
-#
-#         button_record=root.Button(text="Start Recording",command=handle_record)
-#         button_record.place(x=130,y=30)
-
-    
-            
+#     return translated
 
 adharbtn = Button(root,text ='Choose a picture',command = lambda:ask_for_image()) 
 adharbtn.place(x=250,y = 120)
@@ -127,20 +108,12 @@ dlbtn.place(x=450,y = 120)
 msbtn = Button(root,text ='Choose an audio',command = lambda:ask_for_audio()) 
 msbtn.place(x=650,y = 120)
 
-
 trans_btn = Button(root, text = 'Edit',command = ask_for_edit)
 trans_btn.place(x = 800, y= 190 )
 
-# Input_text = Entry(root)
-# Input_text = Text(root, height = 5, wrap = WORD, padx=5, pady=5, width = 60)
-# Input_text.place(x=300,y = 160)
+trans_btn = Button(root, text = 'Translate',command = translate())
+trans_btn.place(x = 450, y= 250 )
 
-
-# trans_btn = Button(root, text = 'Translate',command = translate())
-# trans_btn.place(x = 450, y= 250 )
-
-Input_text2 = Text(root, height = 5, wrap = WORD, padx=5, pady=5, width = 60)
-Input_text2.place(x=300,y = 300)
 
 
 root.mainloop()
--- real_time_translator/translator.py
+++ real_time_translator/translator.py
@@ -1,19 +1,15 @@
 import googletrans
 
-def translat_str(snetence,to_lang):
+def translat_str(snetence,to_language='ar'):
 
     # Initial
     translator = googletrans.Translator()
     # Basic Translate
-    translation = translator.translate( snetence, dest=to_lang)
-    translated=f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})"
+    translation = translator.translate(snetence, dest=to_language)
+    translated = f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})"
     print(translated)
-
-    # with open('abc.txt',mode ='w') as file:     
-    #         file.write(translated)
-
     return translated
 
 if __name__ == "__main__":
     print(translat_str("تفاحة",'en').lower())
-    pass
+    # pass
