0ce5f8c44305513a6b45f528fadebb2c69e36089
Yahia Qous <yahiaqous@gmail.com>
2021-08-24 23:31:15 +0300

    All functionality on page1 are done with classes

--- real_time_translator/translator.py
+++ real_time_translator/translator.py
@@ -5,13 +5,13 @@ def translat_str(snetence,to_language='ar'):
     translation = translator.translate(snetence, dest=to_language)
     translated = f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})"
     
-    with open('real_time_translator/abc.txt',mode ='w') as file:     
-        file.write(translated)
+    # with open('real_time_translator/abc.txt',mode ='w') as file:
+    #     file.write(translated)
     # with open('real_time_translator/abc.txt',mode='r') as file: 
     #     translated = file.readlines()
     
-    file = open('real_time_translator/abc.txt','r')
-    translated = file.read()
+    # file = open('real_time_translator/abc.txt','r')
+    # translated = file.read()
     print(translated)
     return translated
 
